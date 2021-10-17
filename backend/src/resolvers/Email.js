a611fae5007704fc64166c33b1840b24f9973980
halaszd <david.peter.halasz@gmail.com>
2021-10-18 01:13:51 +0200

    Deleted comments and User.js.

--- backend/src/resolvers/Email.js
+++ backend/src/resolvers/Email.js
@@ -7,16 +7,6 @@ function from(parent, args, context) {
     .fromUser()
 }
 
-// function to(parent, args, context) {
-//     return context.prisma.email.findUnique({
-//         where: {
-//             id: parent.id
-//         }
-//     })
-//     .to()
-// }
-
 module.exports = {
     from,
-    // to,
 }
